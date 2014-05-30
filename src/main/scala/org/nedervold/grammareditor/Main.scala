e20204481521e4bc0f84ef13eed4428e8b27f6f6
Eric Nedervold <devnull1999@yahoo.com>
2014-05-29 23:18:11 -0700

    Added an error display.

--- src/main/scala/org/nedervold/grammareditor/Main.scala
+++ src/main/scala/org/nedervold/grammareditor/Main.scala
@@ -1,8 +1,11 @@
 package org.nedervold.grammareditor
 
+import java.awt.Color
 import java.util.concurrent.TimeUnit
+
 import scala.swing.BorderPanel
 import scala.swing.BorderPanel.Position.Center
+import scala.swing.BorderPanel.Position.South
 import scala.swing.BoxPanel
 import scala.swing.Dimension
 import scala.swing.MainFrame
@@ -10,6 +13,12 @@ import scala.swing.Orientation
 import scala.swing.ScrollPane
 import scala.swing.SimpleSwingApplication
 import scala.swing.SplitPane
+import scala.swing.Swing
+import scala.util.Failure
+import scala.util.Success
+import scala.util.Try
+
+import org.nedervold.grammareditor.grammar.Grammar
 import org.nedervold.grammareditor.grammar.GrammarParser
 import org.nedervold.grammareditor.models.DebouncingModel
 import org.nedervold.grammareditor.models.FmapModel
@@ -17,15 +26,13 @@ import org.nedervold.grammareditor.models.Model
 import org.nedervold.grammareditor.models.adapters.DocumentAdapter
 import org.nedervold.grammareditor.models.viewcontrollers.DocumentViewController
 import org.nedervold.grammareditor.models.views.TextAreaView
-import scala.swing.Swing
-import java.awt.Color
-import org.nedervold.grammareditor.grammar.Grammar
 
 object Main extends SimpleSwingApplication {
     System.setProperty("apple.laf.useScreenMenuBar", "true")
 
     val rawGrammarSource = new DocumentAdapter
     val grammarSource = new DebouncingModel(rawGrammarSource, 500, TimeUnit.MILLISECONDS)
+    val grammar: Model[Try[Grammar]] = new FmapModel(GrammarParser.parseGrammar(_: String), grammarSource)
 
     /**
      * Parses the grammar and if it's successful, displays the nonterminals
@@ -33,9 +40,9 @@ object Main extends SimpleSwingApplication {
      * @param source the source of the grammar
      * @return display of the nonterminals
      */
-    def nonterminalsDisplay(source: String): String = {
-        def displayNonterminals(gram: Grammar): String = gram.nonterminals.mkString(", ")
-        GrammarParser.parseGrammar(source).map(displayNonterminals).getOrElse("")
+    def nonterminalsDisplay(gram: Try[Grammar]): String = {
+        def displayNonterminals(g: Grammar): String = g.nonterminals.mkString(", ")
+        gram.map(displayNonterminals).getOrElse("")
     }
 
     /**
@@ -44,9 +51,9 @@ object Main extends SimpleSwingApplication {
      * @param source the source of the grammar
      * @return display of the terminals
      */
-    def terminalsDisplay(source: String): String = {
-        def displayTerminals(gram: Grammar): String = gram.terminals.mkString(", ")
-        GrammarParser.parseGrammar(source).map(displayTerminals).getOrElse("")
+    def terminalsDisplay(gram: Try[Grammar]): String = {
+        def displayTerminals(g: Grammar): String = g.terminals.mkString(", ")
+        gram.map(displayTerminals).getOrElse("")
     }
 
     /**
@@ -54,14 +61,34 @@ object Main extends SimpleSwingApplication {
      *
      * @return a string describing the nonterminals
      */
-    val nonterminalsModel: Model[String] = new FmapModel(nonterminalsDisplay, grammarSource)
+    val nonterminalsModel: Model[String] = new FmapModel(nonterminalsDisplay, grammar)
 
     /**
      * Models a string describing the terminals.  This function is temporary scaffolding.
      *
      * @return a string describing the terminals
      */
-    val terminalsModel: Model[String] = new FmapModel(terminalsDisplay, grammarSource)
+    val terminalsModel: Model[String] = new FmapModel(terminalsDisplay, grammar)
+
+    /**
+     * Parses the grammar and if it's unsuccessful, displays the error message
+     *
+     * @param gram the  grammar attempt
+     * @return the error message
+     */
+    def errorDisplay(gram: Try[Grammar]): String = {
+        gram match {
+            case Success(_) => ""
+            case Failure(ex) => ex.getMessage
+        }
+    }
+
+    /**
+     * Models a string describing parsing errors.  This function is temporary scaffolding.
+     *
+     * @return a string describing parsing errors
+     */
+    val errorModel: Model[String] = new FmapModel(errorDisplay, grammar)
 
     def top = new MainFrame {
         title = "Grammar Editor"
@@ -91,6 +118,7 @@ object Main extends SimpleSwingApplication {
             layout(new SplitPane(Orientation.Vertical, editPanel, infoPanel) {
                 dividerLocation = 600
             }) = Center
+            layout(new TextAreaView(errorModel)) = South
         }
 
         size = new Dimension(800, 600)
